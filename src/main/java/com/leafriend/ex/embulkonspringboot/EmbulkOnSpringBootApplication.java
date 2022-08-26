ab2e7098d200a5e91a243ad340f19a7701b75efa
leafriend <leafriend@gmail.com>
2022-08-26 21:32:23 +0900

    Create project from start.spring.io

---
+++ src/main/java/com/leafriend/ex/embulkonspringboot/EmbulkOnSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.leafriend.ex.embulkonspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmbulkOnSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmbulkOnSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/leafriend/ex/embulkonspringboot/EmbulkOnSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leafriend.ex.embulkonspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmbulkOnSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
